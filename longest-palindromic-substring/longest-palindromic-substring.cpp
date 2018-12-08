6205fa0f276027e755a240bdacfc6e7540dff2a1
yuanhang zheng <yuanhang.zheng@qq.com>
2018-12-08 16:18:56 +0800

    update

--- longest-palindromic-substring/longest-palindromic-substring.cpp
+++ longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
+    string longestPalindromeDp(string s) {
+      vector<vector<bool>> dp(s.size(), vector<bool>(s.size(), false));
+      int maxI = 0, maxJ = 0;
+      for (int i = s.size() - 1; i >= 0; --i) {
+        dp[i][i] = true;
+        for (int j = i + 1; j < s.size(); ++j) {
+          if (s[i] == s[j]) {
+            dp[i][j] = i + 1 >= j - 1 ? true : dp[i + 1][j - 1];
+          }
+          if (dp[i][j] && (j - i) > (maxJ - maxI))
+          {
+            maxI = i;
+            maxJ = j;
+          }
+        }
+        // cout << maxI << " " << maxJ << endl;
+      }
+      return s.substr(maxI, maxJ - maxI + 1);
+    }
+
     string longestPalindrome(string s) {
       string maxStr;
       for (int i = 0; i < s.size(); ++i) {
@@ -36,8 +57,11 @@ int main(void) {
 
   // std::cout << sln.longestPalindrome("") << std::endl;
   std::cout << sln.longestPalindrome("a") << std::endl;
+  std::cout << sln.longestPalindromeDp("a") << std::endl;
   std::cout << sln.longestPalindrome("babad") << std::endl;
+  std::cout << sln.longestPalindromeDp("babad") << std::endl;
   std::cout << sln.longestPalindrome("cbbd") << std::endl;
+  std::cout << sln.longestPalindromeDp("cbbd") << std::endl;
 
   return 0;
 }
